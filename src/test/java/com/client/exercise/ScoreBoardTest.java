a16b97f2ac31266ead12e3df0de6c86b8adc39dd
Giridharan Nanditaraksani <g46362@INTNP.SYSNP.SHARED.FORTIS>
2020-03-03 02:27:19 +0530

    Refactored to extract constant in test class.

--- src/test/java/com/client/exercise/ScoreBoardTest.java
+++ src/test/java/com/client/exercise/ScoreBoardTest.java
@@ -7,10 +7,12 @@ import org.junit.Test;
 
 public class ScoreBoardTest {
 
+	private static final String LOVE_ALL = "Love All";
+
 	@Test
 	public void scoreBoardResultShouldBeLoveAllIfBothPlayersPointsAreZero() {
 		ScoreBoard scoreBoard = new ScoreBoard(0,0);
-		assertThat(scoreBoard.getResult(), CoreMatchers.is("Love All"));
+		assertThat(scoreBoard.getResult(), CoreMatchers.is(LOVE_ALL));
 
 	}
 
